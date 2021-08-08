60b5788a3da1722494ba5ec853f868b12848626b
Jayant <Jayant2410@gmail.com>
2021-08-08 16:06:00 +0530

    Like model

--- social_media/user/models.py
+++ social_media/user/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+
 # Create your models here.
 class Post(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -19,3 +20,18 @@ class Profile(models.Model):
 
     def __str__(self):
         return str(self.user)
+
+class Like(models.Model):
+    user = models.ManyToManyField(User)
+    post = models.OneToOneField(Post, on_delete=models.CASCADE)
+    likes = models.IntegerField(default = 0)
+
+    @classmethod
+    def like(cls, post, liking_user):
+        obj, create = cls.objects.get_or_create(post = post)
+        obj.user.add(liking_user)        
+
+    @classmethod
+    def dislike(cls, post, disliking_user):
+        obj, create = cls.objects.get_or_create(post = post)
+        obj.user.remove(disliking_user)
