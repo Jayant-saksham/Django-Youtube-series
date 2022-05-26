6cf679117377bc3728f43a39dea44325b3dc6df8
Jayant-saksham <Jayant2410@gmail.com>
2022-05-26 22:09:58 +0530

    Ecommerce greatKart

--- Ecommerce/store/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from store.models.category import Category
-from store.models.product import Product
-
-# Register your models here.
-admin.site.register(Category)
-admin.site.register(Product)
--- Ecommerce/store/models/category.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Category(models.Model):
-	category_name = models.CharField(max_length=50, default="Default")
-
-	def __str__(self):
-		return self.category_name
-
-
